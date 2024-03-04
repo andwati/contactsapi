55c9bc0fdbec324c7ac56b77b49c87874eb617aa
Ian Andwati <andwatiian@gmail.com>
2024-03-04 15:16:06 +0300

    created contact model

--- contacts/models.py
+++ contacts/models.py
@@ -4,4 +4,9 @@ from django.db import models
 
 class Contact(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    country_code = models.CharField()
+    country_code = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=30)
+    contact_picture = models.URLField(null=True)
+    is_favorited = models.BooleanField(default=True)
