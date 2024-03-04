5c2a5735257eced8b4b824e731c6e21fca768aaa
Ian Andwati <andwatiian@gmail.com>
2024-03-04 18:01:01 +0300

    bug fix

--- contactsapi/settings.py
+++ contactsapi/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["https://contactsapi-8efb1261fbd1.herokuapp.com/"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
